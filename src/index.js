0988fbcde004e9537e830945de61a53410498984
Mateja9991 <zlatanovic007@gmail.com>
2021-05-05 19:24:36 +0200

    heroku

--- src/index.js
+++ src/index.js
@@ -32,7 +32,7 @@ app.use(express.static(publicPath));
 
 app.use(function (err, req, res, next) {
 	console.error(err);
-	return res.status(500).json({ error: err.message });
+	return res.status(500).json({ error: err });
 });
 
 server.listen(port, () => {
