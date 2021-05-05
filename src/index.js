8b14e5accc10ae7bdc85ee4af3e95e7291924990
Mateja9991 <zlatanovic007@gmail.com>
2021-05-05 17:44:38 +0200

    heroku3

--- src/index.js
+++ src/index.js
@@ -18,6 +18,17 @@ SocketService.initializeSocketServer(server);
 const port = process.env.PORT;
 const publicPath = path.join(__dirname, '../public/');
 
+app.use(function (req, res, next) {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
+	);
+	next();
+});
+
 app.use(express.json());
 app.use(routers);
 app.use(express.static(publicPath));
