bb0b66b16cfeb8669f5f480bca78721cd0c107e9
Mateja9991 <zlatanovic007@gmail.com>
2021-05-22 16:13:15 +0200

    avatar jos

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -85,7 +85,7 @@ async function setAvatarHandler(req, res, next) {
 		req.user.avatar = req.params.avatarId;
 		await req.user.save();
 		// req.user.avatar.picture = await req.user.generateBase64();
-		res.send({ pic: avatar.picture });
+		res.send(avatar);
 	} catch (e) {
 		next(e);
 	}
